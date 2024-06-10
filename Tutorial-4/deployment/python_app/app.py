3f680e7d24577498b8e94e8b76cb17f6c616db66
jwcodeuk <jonathan@iotechsys.com>
2024-06-10 14:51:45 +0100

    update python code

--- Tutorial-4/deployment/python_app/app.py
+++ Tutorial-4/deployment/python_app/app.py
@@ -8,61 +8,102 @@ from sklearn.model_selection import train_test_split
 from flask_socketio import SocketIO, emit
 import paho.mqtt.client as mqtt
 
-allow_unsafe_werkzeug=True
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# SocketIO event handler for client connection
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
 
+# SocketIO event handler for client disconnection
 @socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
 
-
 # MQTT broker details
-broker = "mqtt"  # Replace with your MQTT broker URL
-port = 1883  # Replace with your MQTT broker port
-topic = "xrt/devices/virtual/telemetry"  # Replace with your first MQTT topic
-result_topic = "xrt/devices/s7/request"  # Replace with your result MQTT topic
+broker = "mqtt"
+port = 1883
+topic = "xrt/devices/virtual/telemetry"
+result_topic = "xrt/devices/s7/request"
+
+# Global variables
 is_connected = False
 error_message = ""
 values = [None, None, None]
 alarm = False
 
-# Load the model based on data within safe limits
-
-
-# MQTT callback function
+# MQTT callback function for received messages
 def on_message(client, userdata, msg):
     global values
     global alarm
+
+    # Load the pre-trained model from file
     with open('model.pkl', 'rb') as file:
         loaded_model = joblib.load(file)
+
     if msg.topic == topic:
+        # Extract values from the received message payload
         get_values(msg.payload)
-        if(values[0] is None or values[1] is None):
+
+        # Check if the required values are present
+        if values[0] is None or values[1] is None:
             return
+
+        # Predict the maximum temperature using the loaded model
         max_temperature = loaded_model.predict([[values[0], values[1]]])[0] * 1.4
+
+        # Determine if an alarm should be triggered as the temperature is higher then max
         new_alarm = max_temperature < values[2]
+
+	# if the value of the alarm has changed enter this if statement
         if new_alarm != alarm:
             alarm = new_alarm
-            payload ={
-			"client": "example",
-			"request_id": "1031",
-			"op": "device:put",
-			"type": "xrt.request:1.0",
-			"device": "S7-Server",
-			"values": {
-			    "Alarm": alarm
-			}
-		    }
+
+            # Publish an MQTT message with the alarm value
+            payload = {
+                "client": "example",
+                "request_id": "1031",
+                "op": "device:put",
+                "type": "xrt.request:1.0",
+                "device": "S7-Server",
+                "values": {
+                    "Alarm": bool(alarm)
+                }
+            }
             client.publish(result_topic, json.dumps(payload))
+
+            # Set the error message on website and S7 device
+            #set_error_message(client, "Mill is too hot!")
+        else:
+            # Set the error message on website and S7 device
+            #set_error_message(client, "Good")
+
+        # Emit the updated values and alarm state via SocketIO
         socketio.emit('update_values', {'values': values, 'alarm': bool(new_alarm)})
 
+# Function to set the error message and publish it via MQTT and SocketIO
+def set_error_message(client, msg):
+    # Emit the error message via SocketIO
+    socketio.emit('update_values', {'error_message': msg})
+
+    # Publish the error message via MQTT for XRT to update S7 device with
+    payload = {
+        "client": "example",
+        "request_id": "1031",
+        "op": "device:put",
+        "type": "xrt.request:1.0",
+        "device": "S7-Server",
+        "values": {
+            "Error_Message": str(msg)
+        }
+    }
+    client.publish(result_topic, json.dumps(payload))
+
+# Function to extract values from the received MQTT message payload
 def get_values(payload):
     global values
+
     try:
         message = json.loads(payload)
         if message is not None:
@@ -78,8 +119,7 @@ def get_values(payload):
         print(f"An error occurred while extracting the value: {str(e)}")
         return
 
-
-# The callback for when the client receives a CONNACK response from the server.
+# MQTT callback function for successful connection
 def on_connect(client, userdata, flags, rc):
     global is_connected
     if rc == 0:
@@ -95,18 +135,19 @@ mqttc = mqtt.Client()
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
-
+# Attempt to connect to the MQTT broker
 try:
     mqttc.connect(broker, 1883, 60)
 except ConnectionRefusedError:
     print("Connection to MQTT broker refused. Check if the broker is running.")
 
-
+# Flask route for the root URL
 @app.route('/')
 def index():
     return render_template('index.html', values=values,
                            alarm=alarm, isConnected=is_connected, errorMessage=error_message)
 
+# Run the Flask application with SocketIO
 if __name__ == '__main__':
     mqttc.loop_start()
     socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
