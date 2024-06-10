6475ea80c5fdc86ba0876f45cd1c35f384c4a777
jwcodeuk <jonathan@iotechsys.com>
2024-06-10 13:11:15 +0100

    fix multi command

--- Tutorial-3/opc-ua-server/commands/virtual/multi_put_request.sh
+++ Tutorial-3/opc-ua-server/commands/virtual/multi_put_request.sh
@@ -3,11 +3,12 @@
 mosquitto_pub -t xrt/devices/virtual/request -m \
 '{
   "client": "example",
-  "request_id": "1031",
+  "request_id": "1030",
   "op": "device:put",
   "type": "xrt.request:1.0",
   "device": "Virtual-Device",
   "values": {
-    "StoreInt32Value": 25
+    "StoreInt32Value": 26,
+    "StoreFloat32Value": 3.14
   }
 }'
